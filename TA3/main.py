9b0388a5de7fa3c5d2c90375ed19022348a22849
Samuel Cano <U202116508@upc.edu.pe>
2023-10-22 22:00:32 -0500

    soy el rey del push eduardo

--- TA3/main.py
+++ TA3/main.py
@@ -1,6 +1,7 @@
 import tempfile
 import os
-from flask import Flask, request, redirect, send_file
+from flask import Flask, request, redirect, render_template, url_for, send_file
+
 from skimage import io
 import base64
 import glob
@@ -8,105 +9,11 @@ import numpy as np
 
 app = Flask(__name__)
 
-main_html = """
-<html>
-<head></head>
-<script>
-  var mousePressed = false;
-  var lastX, lastY;
-  var ctx;
-
-   function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-   }
-
-  function InitThis() {
-      ctx = document.getElementById('myCanvas').getContext("2d");
-
-
-      numero = getRndInteger(0, 10);
-      
-      letra=["Cuadrado", "Circulo","Paraguas", "Estrella"];
-      random = Math.floor(Math.random() * letra.length);
-      aleatorio = letra[random];
-
-      document.getElementById('mensaje').innerHTML  = 'Dibujando un ' + aleatorio;
-      document.getElementById('numero').value = aleatorio;
-
-      $('#myCanvas').mousedown(function (e) {
-          mousePressed = true;
-          Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-      });
-
-      $('#myCanvas').mousemove(function (e) {
-          if (mousePressed) {
-              Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-          }
-      });
-
-      $('#myCanvas').mouseup(function (e) {
-          mousePressed = false;
-      });
-  	    $('#myCanvas').mouseleave(function (e) {
-          mousePressed = false;
-      });
-  }
-
-  function Draw(x, y, isDown) {
-      if (isDown) {
-          ctx.beginPath();
-          ctx.strokeStyle = 'black';
-          ctx.lineWidth = 11;
-          ctx.lineJoin = "round";
-          ctx.moveTo(lastX, lastY);
-          ctx.lineTo(x, y);
-          ctx.closePath();
-          ctx.stroke();
-      }
-      lastX = x; lastY = y;
-  }
-
-  function clearArea() {
-      // Use the identity matrix while clearing the canvas
-      ctx.setTransform(1, 0, 0, 1, 0, 0);
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  }
-
-  //https://www.askingbox.com/tutorial/send-html5-canvas-as-image-to-server
-  function prepareImg() {
-     var canvas = document.getElementById('myCanvas');
-     document.getElementById('myImage').value = canvas.toDataURL();
-  }
-
-
-
-</script>
-<body onload="InitThis();">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script type="text/javascript" ></script>
-    <div align="center">
-        <h1 id="mensaje">Dibujando...</h1>
-        <canvas id="myCanvas" width="200" height="200" style="border:2px solid black"></canvas>
-        <br/>
-        <br/>
-        <button onclick="javascript:clearArea();return false;">Borrar</button>
-    </div>
-    <div align="center">
-      <form method="post" action="upload" onsubmit="javascript:prepareImg();"  enctype="multipart/form-data">
-      <input id="numero" name="numero" type="hidden" value="">
-      <input id="myImage" name="myImage" type="hidden" value="">
-      <input id="bt_upload" type="submit" value="Enviar">
-      </form>
-    </div>
-</body>
-</html>
-
-"""
 
 
 @app.route("/")
 def main():
-    return(main_html)
+    return render_template("home.html")
 
 
 @app.route('/upload', methods=['POST'])
@@ -163,4 +70,4 @@ if __name__ == "__main__":
     for d in digits:
         if not os.path.exists(str(d)):
             os.mkdir(str(d))
-    app.run()
+    app.run(debug=True)
